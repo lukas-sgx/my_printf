5940c90220af1e897f0a1598da36dd58fd600732
lukas-sgx <lukas.soigneux@epitech.eu>
2025-10-16 23:34:07 +0200

    [~] Refractor mod h & hh

--- lib/my/core/flags/int.c
+++ lib/my/core/flags/int.c
@@ -10,16 +10,15 @@
 
 int int_d(va_list params, int count, length_mod_t mod)
 {
-
     switch (mod) {
         case MOD_NONE:
             count += my_put_nbr(va_arg(params, int));
             break;
         case MOD_H:
-            count += my_put_nbr((short)(va_arg(params, int)));
+            count += my_put_nbr((short)va_arg(params, int));
             break;
         case MOD_HH:
-            count += my_put_nbr((signed char)(va_arg(params, int)));
+            count += my_put_nbr((signed char)va_arg(params, int));
             break;
         default:
             break;
