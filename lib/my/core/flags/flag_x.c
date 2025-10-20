9ec62c889bdbacc343f82cbdc9cfe4b930728ba3
lukas-sgx <lukas.soigneux@epitech.eu>
2025-10-20 10:14:47 +0200

    [+] Add length modifier -> flag %xX

--- lib/my/core/flags/flag_x.c
+++ lib/my/core/flags/flag_x.c
@@ -9,32 +9,117 @@
 #include "../../include/myprintf.h"
 #include "../../include/my.h"
 
+static void md_ll(va_list params, int *count, length_mod_t mod, char *base_to)
+{
+    unsigned long long arg;
+    char *decimal;
+    char *tmp;
+
+    switch (mod) {
+        case MOD_LL:
+            arg = va_arg(params, unsigned long long);
+            decimal = ullint_to_str(arg);
+            tmp = convert_base(decimal, "0123456789", base_to);
+            *count += my_putstr(tmp);
+            break;
+        default:
+            break;
+    }
+}
+
+static void md_l(va_list params, int *count, length_mod_t mod, char *base_to)
+{
+    unsigned long arg;
+    char *decimal;
+    char *tmp;
+
+    switch (mod) {
+        case MOD_L:
+            arg = va_arg(params, unsigned long);
+            decimal = ulint_to_str(arg);
+            tmp = convert_base(decimal, "0123456789", base_to);
+            *count += my_putstr(tmp);
+            break;
+        default:
+            break;
+    }
+    md_ll(params, count, mod, base_to);
+}
+
+static void md_hh(va_list params, int *count, length_mod_t mod, char *base_to)
+{
+    unsigned int arg;
+    char *decimal;
+    char *tmp;
+
+    switch (mod) {
+        case MOD_HH:
+            arg = va_arg(params, unsigned int);
+            decimal = uint_to_str((unsigned char)arg);
+            tmp = convert_base(decimal, "0123456789", base_to);
+            *count += my_putstr(tmp);
+            break;
+        default:
+            break;
+    }
+    md_l(params, count, mod, base_to);
+}
+
+static void md_h(va_list params, int *count, length_mod_t mod, char *base_to)
+{
+    unsigned int arg;
+    char *decimal;
+    char *tmp;
+
+    switch (mod) {
+        case MOD_H:
+            arg = va_arg(params, unsigned int);
+            decimal = uint_to_str((unsigned short)arg);
+            tmp = convert_base(decimal, "0123456789", base_to);
+            count += my_putstr(tmp);
+            break;
+        default:
+            break;
+    }
+    md_hh(params, count, mod, base_to);
+}
+
 int flag_x_d(va_list params, int count, length_mod_t mod)
 {
-    unsigned int nb = va_arg(params, unsigned int);
+    unsigned int arg;
+    char *decimal;
     char *tmp;
-    char *decimal = uint_to_str(nb);
 
-    if (!decimal)
-        return count;
-    tmp = convert_base(decimal, "0123456789", "0123456789abcdef");
-    free(decimal);
-    count += my_putstr(tmp);
-    free(tmp);
+    switch (mod) {
+        case MOD_NONE:
+            arg = va_arg(params, unsigned int);
+            decimal = uint_to_str(arg);
+            tmp = convert_base(decimal, "0123456789", "0123456789abcdef");
+            count += my_putstr(tmp);
+            break;
+        default:
+            break;
+    }
+    md_h(params, &count, mod, "0123456789abcdef");
     return count;
 }
 
 int flag_xupper_d(va_list params, int count, length_mod_t mod)
 {
-    unsigned int nb = va_arg(params, unsigned int);
+    unsigned int arg;
+    char *decimal;
     char *tmp;
-    char *decimal = uint_to_str(nb);
 
-    if (!decimal)
-        return count;
-    tmp = convert_base(decimal, "0123456789", "0123456789ABCDEF");
-    free(decimal);
-    count += my_putstr(tmp);
-    free(tmp);
+    switch (mod) {
+        case MOD_NONE:
+            arg = va_arg(params, unsigned int);
+            decimal = uint_to_str(arg);
+            tmp = convert_base(decimal, "0123456789", "0123456789abcdef");
+            count += my_putstr(tmp);
+            break;
+        default:
+            break;
+    }
+    md_h(params, &count, mod, "0123456789ABCDEF");
     return count;
 }
