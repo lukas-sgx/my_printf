03c47ed43134a82dcae4ff7bab84ccd0a5c2d783
lukas-sgx <lukas.soigneux@epitech.eu>
2025-10-22 13:54:44 +0200

    [+] Implement format -> flag %i & %d

---
+++ lib/my/core/formats/display_format.c
@@ -0,0 +1,62 @@
+/*
+** EPITECH PROJECT, 2025
+** MyProject
+** File description:
+** display_format.c
+*/
+
+#include "../../../../include/myprintf.h"
+#include "../../../../include/my.h"
+
+static void show_sign(char *sign_char, int pad)
+{
+    if (*sign_char) {
+        my_putchar(*sign_char);
+        *sign_char = 0;
+    }
+    for (int i = 0; i < pad; i++)
+        my_putchar('0');
+}
+
+static void show_pad(int pad)
+{
+    for (int i = 0; i < pad; i++)
+        my_putchar(' ');
+}
+
+static void select_char(char *sign_char, int nb,
+    format_flags_t *format_f, int pad)
+{
+    if (nb < 0)
+        *sign_char = '-';
+    if (format_f->plus && nb > 0)
+        *sign_char = '+';
+    else if (format_f->space)
+        *sign_char = ' ';
+    if (!format_f->minus) {
+        if (format_f->zero) {
+            show_sign(sign_char, pad);
+        } else {
+            show_pad(pad);
+        }
+    }
+}
+
+int display_format(int nb, format_flags_t *format_f, int *count)
+{
+    int len = count_int(nb);
+    int sign = (nb < 0 || format_f->plus || format_f->space)
+        ? 1 : 0;
+    int pad = (format_f->width > len + sign)
+        ? format_f->width - (len + sign) : 0;
+    char sign_char = 0;
+
+    select_char(&sign_char, nb, format_f, pad);
+    if (sign_char)
+        my_putchar(sign_char);
+    *count += my_put_nbr(nb < 0 ? -nb : nb);
+    if (format_f->minus)
+        for (int i = 0; i < pad; i++)
+            my_putchar(' ');
+    return *count;
+}
